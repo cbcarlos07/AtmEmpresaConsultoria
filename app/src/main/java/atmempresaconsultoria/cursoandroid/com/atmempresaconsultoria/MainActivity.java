a0cb23cff429c0939369b1a9c583a0215ec4143f
Carlos Bruno <cbcarlos08@gmail.com>
2017-05-02 14:33:52 -0400

    Cursode Android

--- app/src/main/java/atmempresaconsultoria/cursoandroid/com/atmempresaconsultoria/MainActivity.java
+++ app/src/main/java/atmempresaconsultoria/cursoandroid/com/atmempresaconsultoria/MainActivity.java
@@ -3,12 +3,29 @@ package atmempresaconsultoria.cursoandroid.com.atmempresaconsultoria;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
-
+    ImageView empresa;
+    ImageView servico;
+    ImageView cliente;
+    ImageView contato;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        empresa = (ImageView) findViewById(R.id.empresaId);
+        servico = (ImageView) findViewById(R.id.servicoId);
+        cliente = (ImageView) findViewById(R.id.clienteId);
+        contato = (ImageView) findViewById(R.id.contatoId);
+
+        empresa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
